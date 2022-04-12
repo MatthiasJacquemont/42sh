c9fa224d898db8ec82be8763904745b25a4a46ce
MatthiasJacquemont <94984618+MatthiasJacquemont@users.noreply.github.com>
2022-04-12 17:50:51 +0200

    Delete run_tests.sh
    Deleting the python test suite (not functional)

--- tests/run_tests.sh
+++
@@ -1,8 +0,0 @@
-python -m venv env/ &>/dev/null
-source env/bin/activate &>/dev/null
-pip install termcolor &>/dev/null
-pip install pyyaml &>/dev/null
-
-python tests_suite.py --binary ../builddir/42sh
-
-deactivate
